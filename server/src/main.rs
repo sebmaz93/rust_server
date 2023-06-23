b1cd9511e3454ececcdd68fdf1f401b509e07a61
sebmaz <seb.maz93@icloud.com>
2023-06-23 22:44:28 +0400

    update handlers

--- server/src/main.rs
+++ server/src/main.rs
@@ -29,27 +29,22 @@ async fn main() {
         .build()
         .await
         .expect("Prisma client must start.");
-
     let client_c = Arc::new(client);
     let prisma_filter = warp::any().map(move || client_c.clone());
-
-    let store = store::Store::new();
-    let store_filter = warp::any().map(move || store.clone());
 
     let add_items = warp::post()
         .and(warp::path("v1"))
         .and(warp::path("groceries"))
         .and(warp::path::end())
         .and(post_json_body())
-        .and(store_filter.clone())
         .and(prisma_filter.clone())
-        .and_then(controllers::update_grocery_list);
+        .and_then(controllers::add_grocery_list_item);
 
     let get_items = warp::get()
         .and(warp::path("v1"))
         .and(warp::path("groceries"))
         .and(warp::path::end())
-        .and(store_filter.clone())
+        .and(prisma_filter.clone())
         .and_then(controllers::get_grocery_list);
 
     let update_items = warp::put()
@@ -57,16 +52,15 @@ async fn main() {
         .and(warp::path("groceries"))
         .and(warp::path::end())
         .and(post_json_body())
-        .and(store_filter.clone())
         .and(prisma_filter.clone())
-        .and_then(controllers::update_grocery_list);
+        .and_then(controllers::update_grocery_list_item);
 
     let delete_items = warp::delete()
         .and(warp::path("v1"))
         .and(warp::path("groceries"))
         .and(warp::path::end())
         .and(delete_json_body())
-        .and(store_filter.clone())
+        .and(prisma_filter.clone())
         .and_then(controllers::delete_grocery_list_item);
 
     let routes = add_items.or(get_items).or(update_items).or(delete_items);
