60058e5c50291ec9375bafc1ef2be380a4b80ec1
sebmaz <seb.maz93@icloud.com>
2023-06-24 06:15:53 +0400

    models : seperated

---
+++ server/src/models.rs
@@ -0,0 +1,21 @@
+use crate::db::PrismaClient;
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+
+pub type Db = Arc<PrismaClient>;
+
+pub async fn init_db() -> Db {
+    Arc::new(
+        PrismaClient::_builder()
+            .build()
+            .await
+            .expect("Prisma client must start."),
+    )
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct GroceryItem {
+    pub id: String,
+    pub name: String,
+    pub quantity: i32,
+}
